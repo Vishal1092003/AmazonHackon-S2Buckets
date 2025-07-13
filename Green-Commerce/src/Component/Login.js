1607c70c42d2b92340f42d00b1c7ecebe1f65d06
vishal <codewithme2615@gmail.com>
2025-07-14 03:10:09 +0530

    back to one commit back

--- Green-Commerce/src/Component/Login.js
+++ Green-Commerce/src/Component/Login.js
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://amazon-hackon-s2-buckets.vercel.app/login", {
+      const response = await fetch("http://localhost:8080/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
